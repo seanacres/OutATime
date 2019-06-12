c5c792f15e1bd5f0efe462b76b1838aba78df39c
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-12 16:10:38 -0400

    Renamed some classes and added missing constraints

---
+++ OutATime/DatePickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimePickerViewController.swift
+//  OutATime
+//
+//  Created by Sean Acres on 6/12/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
